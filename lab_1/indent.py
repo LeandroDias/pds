d3e1297e7e0296c6d58ba51785e54443b5e2d11e
Wilder B. Galvao <wilder.galvao@gmail.com>
2017-09-05 23:10:08 -0300

    Add lab 1 codes

---
+++ lab_1/indent.py
@@ -0,0 +1,10 @@
+spam = 2
+
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("How are you?")
+else:
+    print("Greetings!")
+
+print("spam")
---
+++ lab_1/loops.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+for i in np.arange(1, 11):
+    print(i)
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
