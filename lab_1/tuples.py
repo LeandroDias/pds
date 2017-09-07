d5db9eef4a097e17bf1e6783c43d3b96a5bf311f
Wilder B. Galvao <wilder.galvao@gmail.com>
2017-09-07 09:44:52 -0300

    Add tuples example to lab 1

---
+++ lab_1/tuples.py
@@ -0,0 +1,10 @@
+spam = ["cat", "bat", "rat", "elephant"]
+
+print(spam)
+print(type(spam))
+
+spam = spam, ["cat2", "bat2", "rat2", "elephant2"]
+print(spam)
+
+print(spam[1][0])
+print(type(spam))
